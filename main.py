1951f08387969f310ce4b221cc4a3f3ea128ff9c
kimyeonji <yuumi1414@naver.com>
2023-09-20 15:14:45 +0900

    커밋 2

---
+++ main.py
@@ -0,0 +1,14 @@
+def add_func(n1, n2):
+    return n1+n2
+
+def sub_func(n1, n2):
+    return n1-n2
+
+num1 = 100
+num2 = 200
+
+res = add_func(num1, num2)
+print(num1,'+', num2, '=', res)
+
+res = sub_func(num1, num2)
+print(num1, '-', num2, '=', res)
